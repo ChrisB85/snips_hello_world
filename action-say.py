15ad3bf12048278dec9c0de68030de250a89aad6
Krzysztof Błachowicz <krzysztof.blachowicz@gmail.com>
2018-09-16 21:27:23 +0200

    init

---
+++ action-say.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+from hermes_python.hermes import Hermes
+
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
+
+def intent_received(hermes, intent_message):
+    sentence = 'OK, I say '
+
+    if intent_message.intent.intent_name != 'say':
+        return
+
+    word = intent_message.slots.word.first()
+
+    if word is not None:
+        sentence += word.value
+    else:
+        sentence += 'nothing'
+
+    hermes.publish_end_session(intent_message.session_id, sentence)
+
+
+with Hermes(MQTT_ADDR) as h:
+    h.subscribe_intents(intent_received).start()
